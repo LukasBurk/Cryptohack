2abe2b26f9caeb4b1f46569e806635f80c5d75df
Marcel Jacek <marcel.jacek@gmail.com>
2023-11-04 15:32:58 +0100

    Init

--- app/src/main/java/com/example/cryptohack/MainActivity.kt
+++ app/src/main/java/com/example/cryptohack/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.cryptohack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.cryptohack.ui.theme.CryptohackTheme
+import com.example.cryptohack.view.HomeScreen
+import com.example.cryptohack.viewmodel.AssetsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptohackTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/cryptohack/view/HomeScreen.kt
@@ -0,0 +1,88 @@
+package com.example.cryptohack.view
+
+import android.os.CountDownTimer
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.cryptohack.network.CryptoCurrency
+import com.example.cryptohack.viewmodel.AssetsViewModel
+import kotlin.math.roundToInt
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun HomeScreen(viewModel: AssetsViewModel = viewModel()) {
+    val cryptos by viewModel.response.collectAsState()
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "CryptoHack",
+            style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 50.dp)
+        )
+        var showMenu by remember { mutableStateOf(false) }
+        Box(modifier = Modifier.align(Alignment.End)){
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(Icons.Default.MoreVert, "")
+            }
+            DropdownMenu(
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false }) {
+                DropdownMenuItem(text = { Text("Price") }, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text("MarketCap") }, onClick = { /*TODO*/ })
+            }
+        }
+
+        Button(onClick = { viewModel.loadData() }, content = { Text(text = "refresh")})
+
+        LazyColumn {
+            items(cryptos.size, key = { index -> cryptos[index].id }) {
+                index ->
+                CryptoRow(cryptos[index])
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun CryptoRow(cryptoCurrency: CryptoCurrency) {
+    Row (
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth(0.9f)
+    ) {
+        Column {
+            Text(text = cryptoCurrency.name, fontWeight = FontWeight.Bold, style = MaterialTheme.typography.titleLarge)
+            Text(text = cryptoCurrency.symbol, style = MaterialTheme.typography.titleSmall)
+        }
+        Text(((cryptoCurrency.priceUsd * 100).roundToInt().toDouble() /100).toString(), fontWeight = FontWeight.Bold, style = MaterialTheme.typography.titleLarge)
+    }
+}
